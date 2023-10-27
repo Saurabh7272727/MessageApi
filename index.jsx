938fa4c0d2f2aa1dd277996270d4f84d028663e4
saurabh sharma <saurabhsharma63679383@gamil.com>
2023-10-27 22:33:29 +0530

    ok

--- index.jsx
+++ index.jsx
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
 
+app.use(cors());
 app.get('/', (req, res) => {
     res.json({ message: "hello world" })
 });
