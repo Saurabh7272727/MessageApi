3ffa0da930cec3f0df5de5758600da7e7048c377
saurabh sharma <saurabhsharma63679383@gamil.com>
2023-10-27 22:32:08 +0530

    ok

--- index.jsx
+++ index.jsx
@@ -1,1 +1,12 @@
-console.log("hello world!");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json({ message: "hello world" })
+});
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`server listening on ${PORT}`)
+});
